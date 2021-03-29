0acdc6367c734e5481197ec904715c287b4004da
Ivan Ho <ivanhlb@hotmail.com>
2021-03-29 22:23:54 +0800

    Added batch file to facilitate CI

---
+++ test.bat
@@ -0,0 +1,4 @@
+cd "%cd%/Assignment_2"
+python3 ./index.py titles1 ignore1 require1 || python ./index.py titles1 ignore1 require1
+python3 ./index.py titles2 ignore2 require2 || python ./index.py titles2 ignore2 require2
+python3 ./index.py titles3 ignore3 require3 || python ./index.py titles3 ignore3 require3
